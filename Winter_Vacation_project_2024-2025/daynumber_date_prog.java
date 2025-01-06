39fc97f6f6a1f0a4fbc759cf4bad57192cb46d1a
studygear68 <studygear68@gmail.com>
2025-01-06 11:52:30 +0000

    1

--- Winter_Vacation_project_2024-2025/daynumber_date_prog.java
+++ Winter_Vacation_project_2024-2025/daynumber_date_prog.java
@@ -1,69 +1,81 @@
-import java.util.*;
-class daynumber_date_prog
-{
-    int[] m = { 0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
-    String[] mn = { "0", "Jan", "Feb", "Mar", "Apr", "May", "Jun", "July", "Aug", "Sept", "Oct", "Nov", "Dec" };
-    public String check_date(int dn, int y) 
-    {
-        if (y % 4 == 0)
-            m[2] = 29;
-        int s = 0, d =0;
-        String mon = "", date = "";
-        for (int i = 1; i < m.length; i++) 
+import java.util.*;  
+class DayNumberDateProg 
+{  
+   int[] months = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};  
+   String[] monthNames = {"0", "Jan", "Feb", "Mar", "Apr", "May", "Jun", "July", "Aug", "Sept", "Oct", "Nov", "Dec"};  
+   public String checkDate(int dayNumber, int year) 
+   {  
+      if (year % 4 == 0 ) 
+        months[2] = 29;  
+      int sum = 0; 
+      String month = "";  
+      String date = "";  
+      for (int i = 1; i < months.length; i++) 
+      {  
+        if (dayNumber <= months[i]) 
         {  
-            if (dn <= m[i]) 
-            {  
-               mon = mn[i]; 
-               d = dn;  
-               break;  
-            } 
-            else 
-               dn -= m[i];  
-        }
-        if (d == 1||d==21||d==31)
-            date = d + "st";
-        else if (d == 2||d==32)
-            date = d + "nd";
-        if (d == 3||d==23)
-            date = d + "rd";
-        else
-            date = d + "th";
-        return date + " " + mon + " " + y;
-    }
-    public static void main(String[] args) 
-    {
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Day Number = ");
-        int dn = sc.nextInt();
-        System.out.print("Year = ");
-        int y = sc.nextInt();
-        if (y % 4 != 0 && dn == 366) 
-        {
-            System.out.println("Invalid Day Number!");
-            System.exit(0);
-        }
-        System.out.print("Date before (n) = ");
-        int n = sc.nextInt();
-        if (y < 1000 || y > 9999) 
-        {
-            System.out.println("Invalid year");
-            System.exit(0);
-        }
-        if (n > dn) 
-        {
-            System.out.println("Date before " + n + " days does not lie in the same year.");
-            System.exit(0);
-        }
-        if (n < 1 || n > 366) 
-        {
-            System.out.println("Invalid date");
-            System.exit(0);
-        }
-        daynumber_date_prog obj = new daynumber_date_prog();
-        String out1 = obj.check_date(dn, y);
-        System.out.println("Date = " +out1);
-        int newdn = dn - n;
-        String out2 = obj.check_date(newdn, y);
-        System.out.println("Date before " + n + " days = " + out2);
-    }
+           month = monthNames[i];  
+           break;  
+        } 
+        else 
+           dayNumber -= months[i];  
+      }  
+      String suffix;  
+      if (dayNumber >= 11 && dayNumber <= 13)  
+        suffix = "th";  
+      else 
+      {  
+        switch (dayNumber % 10) 
+        {  
+           case 1:  
+              suffix = "st";  
+              break;  
+           case 2:  
+              suffix = "nd";  
+              break;  
+           case 3:  
+              suffix = "rd";  
+              break;  
+           default:  
+              suffix = "th";  
+        }  
+      }  
+      return dayNumber + suffix + " " + month + " " + year;  
+   }  
+   public static void main(String[] args) 
+   {  
+      Scanner scanner = new Scanner(System.in);  
+      System.out.print("Day Number = ");  
+      int dayNumber = scanner.nextInt();  
+      System.out.print("Year = ");  
+      int year = scanner.nextInt();  
+      if (year%4!=0 && dayNumber==366) 
+      {  
+        System.out.println("Invalid Day Number!");  
+        System.exit(0);  
+      }  
+      if (year < 1000 || year > 9999) 
+      {  
+        System.out.println("Invalid year");  
+        System.exit(0);  
+      }  
+      System.out.print("Date before (n) = ");  
+      int n = scanner.nextInt();  
+      if (n < 1 || n > 366) 
+      {  
+        System.out.println("Invalid date");  
+        System.exit(0);  
+      }  
+      if (n > dayNumber) 
+      {  
+        System.out.println("D6ate before " + n + " days does not lie in the same year.");  
+        System.exit(0);  
+      }  
+      DayNumberDateProg obj = new DayNumberDateProg();  
+      String output1 = obj.checkDate(dayNumber, year);  
+      System.out.println("Date = " + output1);  
+      int newDayNumber = dayNumber - n;  
+      String output2 = obj.checkDate(newDayNumber, year);  
+      System.out.println("Date before " + n + " days = " + output2);  
+   }  
 }
